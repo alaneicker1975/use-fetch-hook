832109a96c30ebda312d2022b6c21aa070c62c3f
Alan Eicker <alaneicker@gmail.com>
2021-09-21 09:00:26 -0500

    Updates app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { useFetch } from './useFetch';
+import Overlay from '@atomikui/core/dist/components/overlay';
+import Spinner from '@atomikui/core/dist/components/spinner';
+import Alert from '@atomikui/core/dist/components/alert';
+import List from '@atomikui/core/dist/components/list';
 
 const App = () => {
   const { isLoading, data, error } = useFetch(
     'https://jsonplaceholder.typicode.com/users',
   );
 
-  if (isLoading) return 'Loading...';
-  if (error) return error;
+  if (isLoading)
+    return (
+      <Overlay isActive>
+        <Spinner size="xlg" theme="white" themeVariant="light" />
+      </Overlay>
+    );
 
-  return <pre className="App">{JSON.stringify(data, null, 2)}</pre>;
+  if (error) return <Alert theme="error">{error}</Alert>;
+
+  return (
+    <List>
+      {data.map(({ id, name }) => (
+        <List.Item key={id}>{name}</List.Item>
+      ))}
+    </List>
+  );
 };
 
 export default App;
